66e5e3ec628d46473f9c0293451d31c5dc107aeb
Luke Harrison <luke@archcreate.com>
2011-09-21 10:10:18 +0100

    User model with passwords, and user show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                   "Luke Harrison"
+  user.email                  "luke@archcreate.com"
+  user.password               "foobar"
+  user.password_confirmation  "foobar"
+end
